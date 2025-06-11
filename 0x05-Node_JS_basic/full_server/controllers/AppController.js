aeccb193feaa4cebf55720343f4390159483f3d7
MohShalaby92 <mohammedshalaby92@gmail.com>
2025-06-11 11:11:01 +0000

    nodejs

---
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -0,0 +1,8 @@
+class AppController {
+  static getHomepage(request, response) {
+    response.status(200).send('Hello ALX!');
+  }
+}
+
+export default AppController;
+module.exports = AppController;
